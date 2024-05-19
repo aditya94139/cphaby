dbfdb22f1d53b4525b3050c0f8b257a0b32cc4fa
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-05-19 15:37:23 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 28528007
-api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "6706535540:AAF27SlXPmgl2NZtC0lP_Ql9ZtPf7nMBrSk"
-auth_users = [6329158981]
-sudo_user = 6329158981
-log_channel = -1002024552188
-txt_channel = -1002010646599
+api_id = 25586552
+api_hash = "f265cba9d76dc6ad70914accbe758f47"
+bot_token = "7169852715:AAH7cj08TU0zRiSE-5mhtWPJpL72mPwQuLY"
+auth_users = [1368753935]
+sudo_user = 1368753935
+log_channel = -1002137333016
+txt_channel = -1002084609855
